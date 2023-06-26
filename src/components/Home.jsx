f262bd4a91eb6191a6bcbf812c044e2a16279591
furqonstudio <furqonstudio@gmail.com>
2023-06-26 08:18:00 +0700

    Fix : move images to public folder

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -23,24 +23,24 @@ const Home = () => {
 
       <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 lg:mx-40 md:mx-20 gap-6">
         <Link to="/actions">
-          <img className="img-menu " src="/src/images/Frame1.jpg" alt="menu1" />
+          <img className="img-menu " src="/images/Frame1.jpg" alt="menu1" />
         </Link>
         <Link to="/data-display">
-          <img className="img-menu " src="/src/images/Frame2.jpg" alt="menu2" />
+          <img className="img-menu " src="/images/Frame2.jpg" alt="menu2" />
         </Link>
 
         <Link to="/data-input">
-          <img className="img-menu" src="/src/images/Frame3.jpg" alt="menu3" />
+          <img className="img-menu" src="/images/Frame3.jpg" alt="menu3" />
         </Link>
 
         <Link to="/layout">
-          <img className="img-menu " src="/src/images/Frame4.jpg" alt="menu4" />
+          <img className="img-menu " src="/images/Frame4.jpg" alt="menu4" />
         </Link>
         <Link to="/navigation">
-          <img className="img-menu" src="/src/images/Frame5.jpg" alt="menu5" />
+          <img className="img-menu" src="/images/Frame5.jpg" alt="menu5" />
         </Link>
         <Link to="/">
-          <img className="img-menu" src="/src/images/Frame6.jpg" alt="menu6" />
+          <img className="img-menu" src="/images/Frame6.jpg" alt="menu6" />
         </Link>
       </div>
       <Footer />
